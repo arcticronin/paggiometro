2935d7956caacaf12eb3c7152666f22f0734e2e9
mrRonin <ronin.git@gmail.com>
2019-01-23 01:43:57 +0100

    continued

--- paggiometro.py
+++ paggiometro.py
@@ -1,24 +1,33 @@
 from tkinter import *
-from . import backend.py
+from backend import *
+from tk_tools import *
 
 window=Tk()
 
-l1=Label(window, text="PaggioMetro")
-l1.grid(row=0,column=0)
+led1 = Led(window, size=50)
+led1.grid(row=0,column=0,pady=(20,10))
+led1.to_grey()
 
-l2=Label(window, text="V-Inserisci Nome-V")
-l2.grid(row=1,column=0)
+paggezza_text=StringVar()
+e1=Entry(window,textvariable=paggezza_text)
+e1.grid(row=0,column=1,padx=(20,10))
 
-title_text=StringVar()
-e1=Entry(window,textvariable=title_text)
-e1.grid(row=2,column=0)
 
-b1=Button(window,text="Calcola",width=12)
-b1.grid(row=3,column=0)
+gauge= Gauge(window, max_value=100.0, label='Paggezza', unit='pg',
+            height=150, width=250,divisions=20,yellow=30,bg=None)
+gauge.grid(row=1,column=0,columnspan=2)
+gauge.set_value(70)
 
-title_text=StringVar()
-e2=Entry(window,textvariable=title_text2)
-e2.grid(row=4,column=0)
+
+l2=Label(window, text="V  Inserisci Nome  V")
+l2.grid(row=2,column=0, columnspan=2, pady=(15,10))
+
+ricerca=StringVar()
+e2=Entry(window,textvariable=ricerca)
+e2.grid(row=3,column=0, columnspan=2)
+
+b1=Button(window,text="Calcola Paggezza",width=20,font=("monospace",15,"bold"))
+b1.grid(row=4,column=0,columnspan=2, pady=15, padx=10)
 
 
 
