d08016f718b47aef1e0d546c749e3cc125443699
mrRonin <ronin.git@gmail.com>
2019-01-25 11:54:38 +0100

    same

--- backend.py
+++ backend.py
@@ -1,2 +1,44 @@
-def title_text2():
-    return ("############")
+import sqlite3
+import random
+
+
+def connect():
+    conn = sqlite3.connect('paggi.db')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS paggio(id INTEGER PRIMARY KEY, nome \
+                TEXT, pag INTEGER) ')
+    conn.commit()
+    conn.close()
+
+
+""" ANNULLATA per test
+
+
+def ricerca():
+    conn = sqlite3.connect("paggi.db")
+    cur = conn.cursor()
+    # linea modificta, n era passato alla funzione
+    n = paggezza_text.get()
+    # fine modifica
+    cur.execute("SELECT pag FROM paggi WHERE nome = ? ", n)
+    num = int(cur.fetchone())
+    conn.close()
+    if (num == ""):
+        return random.randint(0, 100)
+    return num
+
+"""
+
+
+def insert(nome, numero):
+    conn = sqlite3.connect("paggi.db")
+    cur = conn.cursor()
+    cur.execute('INSERT INTO paggio VALUES(NULL,?,?)', (nome, numero))
+    conn.commit()
+    conn.close()
+
+
+connect()
+insert("luca", 10)
+insert("giulia", 30)
+insert("lella", 100)
--- paggiometro.py
+++ paggiometro.py
@@ -1,34 +1,66 @@
 from tkinter import *
-from backend import *
+import backend
 from tk_tools import *
 
-window=Tk()
+
+def esegui():
+    nome = paggezza_text.get()
+    pgg = backend.ricerca(nome)
+    gauge.set_value(pgg)
+    if (pgg < 30):
+        led1.to_green
+    elif (pgg < 50):
+        led1.to_yellow
+    else:
+        led1.to_red
+
+
+window = Tk()
 
 led1 = Led(window, size=50)
-led1.grid(row=0,column=0,pady=(20,10))
+led1.grid(row=0, column=0, pady=(20, 10))
 led1.to_grey()
 
-paggezza_text=StringVar()
-e1=Entry(window,textvariable=paggezza_text)
-e1.grid(row=0,column=1,padx=(20,10))
+paggezza_num = StringVar()
+e1 = Entry(window, textvariable=paggezza_num)
+e1.grid(row=0, column=1, padx=(20, 10))
 
 
-gauge= Gauge(window, max_value=100.0, label='Paggezza', unit='pg',
-            height=150, width=250,divisions=20,yellow=30,bg=None)
-gauge.grid(row=1,column=0,columnspan=2)
-gauge.set_value(70)
+gauge = Gauge(window, max_value=100.0, label='Paggezza', unit='pg',
+              height=150, width=250, divisions=20, yellow=30, bg=None)
+gauge.grid(row=1, column=0, columnspan=2)
+
+l2 = Label(window, text='V  Inserisci Nome  V')
+l2.grid(row=2, column=0, columnspan=2, pady=(15, 10))
+
+paggezza_text = StringVar()
+e2 = Entry(window, textvariable=paggezza_text)
+e2.grid(row=3, column=0, columnspan=2)
+
+b1 = Button(window, text='Calcola Paggezza', width=20, command=esegui,
+            font=("monospace", 15, "bold"))
+b1.grid(row=4, column=0, columnspan=2, pady=15, padx=10)
+
+gauge.set_value(96)
+led1.to_red()
 
 
-l2=Label(window, text="V  Inserisci Nome  V")
-l2.grid(row=2,column=0, columnspan=2, pady=(15,10))
+def ricerca(nome):
+    conn = backend.sqlite3.connect("paggi.db")
+    cur = conn.cursor()
+    nome = paggezza_text.get()
+    # cheat VVV
+    lu = 'luca'
+    cur.execute("SELECT pag FROM paggio WHERE nome = ? ", lu)
+    num = int(cur.fetchone())
+    conn.close()
+    if (num == ""):
+        return random.randint(0, 100)
+    return num
 
-ricerca=StringVar()
-e2=Entry(window,textvariable=ricerca)
-e2.grid(row=3,column=0, columnspan=2)
 
-b1=Button(window,text="Calcola Paggezza",width=20,font=("monospace",15,"bold"))
-b1.grid(row=4,column=0,columnspan=2, pady=15, padx=10)
+uno = ricerca('luca')
 
-
+print(uno)
 
 window.mainloop()
